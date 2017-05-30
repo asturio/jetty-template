0eb86dd56425cc54e3c728fe1c1cb7a73de480b4
Claudio Clemens <asturio@gmx.net>
2017-05-30 23:40:01 +0200

    Added SSL Port to script

--- startJetty.sh
+++ startJetty.sh
@@ -6,8 +6,9 @@ JETTY_HOME=/opt/apps/jetty
 # Directory containing the separated from JETTY_HOME
 JETTY_BASE=$1
 JETTY_PORT=8080
+JETTY_SSL_PORT=8443
 
 
 cd $JETTY_BASE || (echo "$JETTY_BASE doesn't exist!" && exit 1)
 
-java -jar $JETTY_HOME/start.jar jetty.http.port=$JETTY_PORT
+java -jar $JETTY_HOME/start.jar jetty.http.port=$JETTY_PORT jetty.ssl.port=$JETTY_SSL_PORT
