5657606cfc8d6d81f8792cb290031f00e9278194
Claudio Clemens <asturio@gmx.net>
2017-05-30 23:56:00 +0200

    Stop working.

--- stopJetty.sh
+++ stopJetty.sh
@@ -5,11 +5,11 @@ JETTY_HOME=/opt/apps/jetty
 
 # Directory containing the separated from JETTY_HOME
 JETTY_BASE=$1
-JETTY_PORT=8080
 JETTY_STOP_PORT=8081
 JETTY_STOP_KEY=killMePlease
 
+# Can be avoided. Maybe read configuration from directory JETTY_BASE
 cd $JETTY_BASE || (echo "$JETTY_BASE doesn't exist!" && exit 1)
 
-# Stop Key must be keeped secret
-java -jar $JETTY_HOME/start.jar STOP.PORT=$JETTY_STOP_PORT STOP.KEY=$JETTY_STOP_KEY jetty.http.port=$JETTY_PORT
+java -jar $JETTY_HOME/start.jar STOP.PORT=$JETTY_STOP_PORT STOP.KEY=$JETTY_STOP_KEY --stop
+
