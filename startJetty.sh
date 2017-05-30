9fc81cf0bd2a62564d1a93245adca7f88b8fac84
Claudio Clemens <asturio@gmx.net>
2017-05-30 23:19:46 +0200

    Added port as parameter.

--- startJetty.sh
+++ startJetty.sh
@@ -5,8 +5,9 @@ JETTY_HOME=/opt/apps/jetty
 
 # Directory containing the separated from JETTY_HOME
 JETTY_BASE=$1
+JETTY_PORT=8080
 
 
 cd $JETTY_BASE || (echo "$JETTY_BASE doesn't exist!" && exit 1)
 
-java -jar $JETTY_HOME/start.jar
+java -jar $JETTY_HOME/start.jar jetty.http.port=$JETTY_PORT
